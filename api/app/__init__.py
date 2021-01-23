14f7f788afb1ca46197b7628d429b8962c818adf
Vadym Kinchur <vkincur@gmail.com>
2021-01-23 22:53:42 +0200

    Rebuild project layout

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 from flask_socketio import SocketIO
 from config import config
 
-socketio = SocketIO()
+sio = SocketIO()
 
 
 def create_app(config_name: str) -> Flask:
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-    socketio.init_app(app)
+    sio.init_app(app)
     return app
---
+++ api/app/schema.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE TABLE users (
+	id serial PRIMARY KEY,
+	name varchar(128) NOT NULL,
+	username varchar(128) UNIQUE NOT NULL,
+	status varchar(128),
+	entered_date timestamp
+);
+
+CREATE OR REPLACE TABLE users_auth (
+	id serial PRIMARY KEY,
+	user_id int REFERENCES user (user_id) NOT NULL,
+	email varchar(255) UNIQUE NOT NULL,
+	password varchar(128) NOT NULL
+);
+
+CREATE OR REPLACE chats (
+	id serial PRIMARY KEY,
+	title varchar(32) NOT NULL,
+	description varchar(128)
+);
+
+CREATE OR REPLACE TABLE users_chats (
+	user_id int REFERENCES users_auth (id),
+	chat_id int REFERENCES chats (id)
+);
+
+CREATE OR REPLACE TABLE messages (
+	author_id int REFERENCES users_auth (id) NOT NULL,
+	chat_id int REFERENCES chats (id)
+	content text,
+	PRIMARY KEY (author_id, chat_id)
+);
+
